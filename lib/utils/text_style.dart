0b50ae3c0344ca6a317b69cd4f228051301903c6
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-12 20:15:34 +0530

    landing page UI done

--- lib/utils/text_style.dart
+++ lib/utils/text_style.dart
@@ -7,30 +7,35 @@ class TextStyles{
     fontSize: 26,
     fontWeight: FontWeight.w800,
     color: Colors.white.withOpacity(0.7),
+    fontFamily: 'Ubuntu',
   );
 
   static final TextStyle tagLineTextStyle = TextStyle(
     fontSize: 12,
     fontWeight: FontWeight.w600,
     color: Colors.white,
+    fontFamily: 'Ubuntu',
   );
 
   static final TextStyle bigHeadingTextStyle = TextStyle(
     fontSize: 60,
     fontWeight: FontWeight.w900,
     color: Colors.white,
+    fontFamily: 'Ubuntu',
   );
 
   static final TextStyle bodyTextStyle = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w400,
     color: Colors.white,
+    fontFamily: 'Ubuntu',
   );
 
   static final TextStyle buttonTextStyle = TextStyle(
     fontSize: 22,
     fontWeight: FontWeight.w600,
     color: Colors.white,
+    fontFamily: 'Ubuntu',
   );
 
 
